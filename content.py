3fcc4197334d31224f2217d0380f31e5d38fde3a
maaaaagda <magdapolak96@gmail.com>
2017-05-20 15:50:30 +0200

    Done

--- content.py
+++ content.py
@@ -47,6 +47,8 @@ def sort_train_labels_knn(Dist, y):
     n2 = len(y)
 
     w = np.zeros(shape=(n1, n2))
+    print(y)
+
     for i in range(0, n1):
         w[i, :] = y[Dist[i, :].argsort(kind='mergesort')]
     return w
@@ -55,7 +57,7 @@ def sort_train_labels_knn(Dist, y):
 def p_y_x_knn(y, k):
     """
     Funkcja wyznacza rozklad prawdopodobienstwa p(y|x) dla
-    kazdej z klas dla obiektow ze zbioru testowego wykorzystujac
+    kazdej z klas dla obiektow ze  zbioru testowego wykorzystujac
     klasfikator KNN wyuczony na danych trenningowych
     :param y: macierz posortowanych etykiet dla danych treningowych N1xN2
     :param k: liczba najblizszuch sasiadow dla KNN
@@ -87,14 +89,11 @@ def classification_error(p_y_x, y_true):
     Kazdy wiersz macierzy reprezentuje rozklad p(y|x)
     :return: blad klasyfikacji
     """
-    print(p_y_x)
+
     n1 = p_y_x.shape[0]
-    n2 = p_y_x.shape[1]
-    w = np.argsort(p_y_x)+1
-    q = 0
-    for e1, e2 in zip(w[:,n2-1], y_true):
-        if e1 != e2:
-            q+=1
+    p_y_x = np.fliplr(p_y_x)
+    r = p_y_x.shape[1] - np.argmax(p_y_x, axis=1)   #ostatnie wystapienie
+    q = np.count_nonzero(r-y_true)
     return q/n1
 
 
@@ -110,16 +109,16 @@ def model_selection_knn(Xval, Xtrain, yval, ytrain, k_values):
     """
     y = sort_train_labels_knn(hamming_distance(Xval, Xtrain), ytrain)
     l = len(k_values)
-    summary = np.zeros(shape=(l, 2))
+    errors = []
+    best_error = 10
+    best_k = -1
     for k in range (len(k_values)):
         pxy = p_y_x_knn(y, k_values[k])
         e = classification_error(pxy, yval)
-        summary[k] = [e, k_values[k]]
-
-    best_error_i = np.argsort(summary[:,0])[0]
-    best_error = summary[best_error_i, 0]
-    best_k = summary[best_error_i, 1]
-    errors = summary[:,0]
+        if(e<best_error):
+            best_error = e
+            best_k = k_values[k]
+        errors.append(e)
 
     return (best_error, best_k, errors)
 
@@ -143,13 +142,17 @@ def estimate_p_x_y_nb(Xtrain, ytrain, a, b):
     :param b: parametr b rozkladu Beta
     :return: funkcja wyznacza rozklad prawdopodobienstwa p(x|y) zakladajac, ze x przyjmuje wartosci binarne i ze elementy
     x sa niezalezne od siebie. Funkcja zwraca macierz p_x_y o wymiarach MxD.
-    """
+    """"""
 
     n1 = Xtrain.shape[0]        #50
     n2 = Xtrain.shape[1]        #20
     q = np.unique(ytrain)       #klasy
     w = len(q)                  #ilosc klas
     k = np.zeros(shape=(n2, w))
+    d = Counter(ytrain) # zliczam ilosci poszczegolnych etykietkow potem bede przez nie dzieliła
+    t = sorted(d.items(), key=lambda x: x[0])
+    h = np.array([tt[1] for tt in t])
+    print(h)
     for i in range(n2):
         u = []
         for j in range(n1):
@@ -157,11 +160,27 @@ def estimate_p_x_y_nb(Xtrain, ytrain, a, b):
             if(x!=0):
                 u.append(x)
         c = Counter(u)
-        for z in range(1, w + 1):       #dodaje te klasy, ktore mogly nie zostac wylapane
+        for z in range(1, w + 1):       #dodaje te klasy, ktore mogly nie zostac wylapane, moze nikonieczne
             c.update({z: 0})
-        s = sorted(c.items(), key=lambda x: x[0])   #sortuje po kluczach - klasach
-        k[i]=[cc[1] for cc in s]
-    return(k.T+a-1) / (n1+a+b-2)
+            d.update({z: 0})
+        s = sorted(c.items(), key=lambda x: x[0])         #sortuje po kluczach - klasach
+        k[i]=[ss[1] for ss in s]
+        k[i]=np.divide((k[i]+a-1),(h+a+b-2))
+    return k.T"""
+    Xtrain = Xtrain.toarray()
+    NUMBER_OF_CLASSES = 4
+    rows = []
+
+    def summ(row, yequalk):
+        return np.sum(np.bitwise_and(row, yequalk))
+
+    for i in range(1, NUMBER_OF_CLASSES + 1):
+        yk = np.equal(ytrain, i)
+        yksum = np.sum(yk)          #ilosc wystapien danej klasy
+        row = np.apply_along_axis(summ, axis=0, arr=Xtrain, yequalk=yk) #dla danej klasy zlicza ilosc jedyneczek
+        rows.append(np.divide(np.add(row, a - 1), yksum + a + b - 2))
+
+    return np.vstack(rows)
 
 
 def p_y_x_nb(p_y, p_x_1_y, X):
@@ -172,7 +191,26 @@ def p_y_x_nb(p_y, p_x_1_y, X):
     :return: funkcja wyznacza rozklad prawdopodobienstwa p(y|x) dla kazdej z klas z wykorzystaniem klasyfikatora Naiwnego
     Bayesa. Funkcja zwraca macierz p_y_x o wymiarach NxM.
     """
-    pass
+    X = X.toarray()
+
+    def normalise(row):
+        Z = 1 / np.sum(row)
+        return np.multiply(row, Z)
+
+    def calc(row):
+        out = X * row
+        out += np.negative(X) - np.negative(X) * row
+        out = np.apply_along_axis(np.prod, arr=out, axis=1)
+        return out
+
+    def calc2(row, x2):
+        return np.multiply(row, x2)
+
+    test = np.apply_along_axis(calc, axis=0, arr=p_x_1_y.transpose())
+    test = np.apply_along_axis(calc2, axis=1, arr=test, x2=p_y)
+    test = np.apply_along_axis(normalise, axis=1, arr=test)
+
+    return test
 
 
 def model_selection_nb(Xtrain, Xval, ytrain, yval, a_values, b_values):
@@ -188,7 +226,47 @@ def model_selection_nb(Xtrain, Xval, ytrain, yval, a_values, b_values):
     osiagniety blad, best_a - a dla ktorego blad byl najnizszy, best_b - b dla ktorego blad byl najnizszy,
     errors - macierz wartosci bledow dla wszystkich par (a,b)
     """
-    pass
+    A = len(a_values)
+    B = len(b_values)
+    error_best = 2
+    errors = np.zeros(shape=(A, B), dtype=np.float64)
+    best_a = 0
+    best_b = 0
+
+    for a in range(A):
+        for b in range(B):
+            p_y = np.squeeze(np.asarray(estimate_a_priori_nb(ytrain)))
+            p_x_1_y = estimate_p_x_y_nb(Xtrain, ytrain, a_values[a], b_values[b])
+            p_y_x = p_y_x_nb(p_y, p_x_1_y, Xval)
+            error = classification_error(p_y_x, yval)
+            errors[a, b] = error
+            if error < error_best:
+                error_best = error
+                best_a = a
+                best_b = b
+
+    return error_best, a_values[best_a], b_values[best_b], errors
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 """
 a = np.array([[1,1]])
 b = np.array([[0,1]])
